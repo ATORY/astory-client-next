4276d19e2ce663703ecf3bfcc6f46386a27f0c67
veryci-ttang <guchuantang@verci.com>
2017-08-16 17:14:10 +0800

    gzip add

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const next = require('next');
 const winston = require('winston');
 const config = require('config');
+const compression = require('compression');
 
 const SERVER_CONFIG = config.get('server');
 const dev = process.env.NODE_ENV !== 'production';
@@ -10,7 +11,7 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
-
+  server.use(compression());
   server.get('/article/:articleId', (req, res) => {
     const actualPage = '/article';
     const queryParams = { articleId: req.params.articleId };
